3a081d51eec0f020c3a169e31bb99e1fc7e9f886
glegshot <sreekumar.y2k@gmail.com>
2021-06-12 12:10:03 +0530

    game loop and basic rendering setup

--- src/main/java/org/tutorial/game/main/Game.java
+++ src/main/java/org/tutorial/game/main/Game.java
@@ -1,6 +1,7 @@
 package org.tutorial.game.main;
 
 import java.awt.*;
+import java.awt.image.BufferStrategy;
 
 public class Game extends Canvas implements Runnable {
 
@@ -9,13 +10,27 @@ public class Game extends Canvas implements Runnable {
     private static final int WIDTH = 640;
     private static final int HEIGHT = 640 / 12 * 9;
 
+    private Thread thread;
+    private boolean isRunning;
+
 
     public Game(){
         new Window(WIDTH, HEIGHT, "Let's Build A Game", this);
     }
 
     public synchronized void start(){
+        thread = new Thread(this);
+        thread.start();
+        isRunning = true;
+    }
 
+    public synchronized void stop(){
+        try {
+            thread.join();
+            isRunning = false;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
@@ -24,6 +39,49 @@ public class Game extends Canvas implements Runnable {
 
     @Override
     public void run() {
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int frames = 0;
+        while(isRunning){
+            long now = System.nanoTime();
+            delta += (now - lastTime)/ns;
+            lastTime = now;
+            while(delta >= 1){
+                tick();
+                delta--;
+            }
+            if(isRunning){
+                render();
+            }
+            frames++;
 
+            if(System.currentTimeMillis() - timer > 1000){
+                timer += 1000;
+                System.out.println("FPS : "+frames);
+                frames = 0;
+            }
+        }
+        stop();
+    }
+
+    private void render() {
+        BufferStrategy bufferStrategy = this.getBufferStrategy();
+        if(bufferStrategy == null){
+            this.createBufferStrategy(3);
+            return;
+        }
+
+        Graphics graphics = bufferStrategy.getDrawGraphics();
+        graphics.setColor(Color.BLACK);
+        graphics.fillRect(0,0,WIDTH,HEIGHT);
+
+        graphics.dispose();
+        bufferStrategy.show();
+    }
+
+    private void tick() {
     }
 }
