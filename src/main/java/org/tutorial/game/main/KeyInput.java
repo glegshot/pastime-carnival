e3150737dc3b40c1c276b7ed11730d36d297f3f1
glegshot <sreekumar.y2k@gmail.com>
2021-06-14 12:47:22 +0530

    added jump with walk logic

--- src/main/java/org/tutorial/game/main/KeyInput.java
+++ src/main/java/org/tutorial/game/main/KeyInput.java
@@ -2,7 +2,6 @@ package org.tutorial.game.main;
 
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.security.Key;
 import java.util.stream.Collectors;
 
 public class KeyInput extends KeyAdapter {
@@ -17,27 +16,43 @@ public class KeyInput extends KeyAdapter {
     public void keyPressed(KeyEvent e) {
        Player player = (Player) this.handler.gameObjectList.stream().filter(gameObject -> gameObject.id == ID.Player).collect(Collectors.toList()).get(0);
        if(e.getKeyCode() == KeyEvent.VK_W){
-           ///player.setVelY(-5);
-       }else if(e.getKeyCode() == KeyEvent.VK_S){
-           //player.setVelY(5);
-       }else if(e.getKeyCode() == KeyEvent.VK_A){
-           //player.setVelX(-5);
-       }else if(e.getKeyCode() == KeyEvent.VK_D){
-           //player.setVelX(5);
+           if(!(player.isJump || player.isFall)) {
+               System.out.println("inside w press");
+               player.setVelY(-2);
+               player.isJump = true;
+               player.isFall = false;
+               printCoordinates(player);
+           }
        }
+       if(e.getKeyCode() == KeyEvent.VK_S){
+           printCoordinates(player);
+       }
+       if(e.getKeyCode() == KeyEvent.VK_A){
+           player.setVelX(-5);
+           printCoordinates(player);
+       }
+       if(e.getKeyCode() == KeyEvent.VK_D){
+           player.setVelX(5);
+           printCoordinates(player);
+       }
+    }
+
+    public void printCoordinates(Player player){
+        System.out.println("x: "+player.getX()+", y: "+player.getY());
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
         Player player = (Player) this.handler.gameObjectList.stream().filter(gameObject -> gameObject.id == ID.Player).collect(Collectors.toList()).get(0);
         if(e.getKeyCode() == KeyEvent.VK_W){
-            //player.setVelY(0);
-        }else if(e.getKeyCode() == KeyEvent.VK_S){
-            //player.setVelY(0);
-        }else if(e.getKeyCode() == KeyEvent.VK_A){
-            //player.setVelX(0);
-        }else if(e.getKeyCode() == KeyEvent.VK_D){
-            //player.setVelX(0);
+        }
+        if(e.getKeyCode() == KeyEvent.VK_S){
+        }
+        if(e.getKeyCode() == KeyEvent.VK_A){
+            player.setVelX(0);
+        }
+        if(e.getKeyCode() == KeyEvent.VK_D){
+            player.setVelX(0);
         }
     }
 }
